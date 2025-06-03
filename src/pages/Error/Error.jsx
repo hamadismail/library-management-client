164f6e61da4ea2577130faa345296011ce4ab039
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-06-04 04:17:48 +0600

    implement error page

---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link, useRouteError } from "react-router";
+
+const Error = () => {
+  const error = useRouteError();
+
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="text-center">
+        <h1 className="mb-8 text-7xl font-barlow-thin text-gray-900">
+          {error?.status || 404}
+        </h1>
+        <p className="mb-3 text-xl font-barlow-bold text-gray-900 md:text-2xl">
+          {error?.error?.message || "Something Went Wrong!"}
+        </p>
+        <Link className="btn" to="/">
+          Go To Homepage
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Error;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home/Home";
+import Error from "../pages/Error/Error";
 
 export const router = createBrowserRouter([
   {
     path: "/",
+    errorElement: <Error />,
     Component: MainLayout,
     children: [{ index: true, Component: Home }],
   },
