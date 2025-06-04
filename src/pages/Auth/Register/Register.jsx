8884f577e4a2f944a042d1db4209b07c6557e0e8
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-06-04 11:04:12 +0600

    implement react hook form

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import toast, { Toaster } from "react-hot-toast";
 import {
   FaEnvelope,
@@ -12,20 +13,25 @@ import { Link } from "react-router";
 
 const Register = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const handleRegister = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const email = form.email.value;
-    const password = form.password.value;
+    // e.preventDefault();
+    // const form = e.target;
+    // const email = form.email.value;
+    // const password = form.password.value;
+    // const pattern = /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+    // const isValidate = pattern.test(password);
+    // if (!isValidate)
+    //   return toast.error(
+    //     "Password Must be 6 character long and a uppercase letter and a lowercase letter"
+    //   );
 
-    const pattern = /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
-    const isValidate = pattern.test(password);
-
-    if (!isValidate)
-      return toast.error(
-        "Password Must be 6 character long and a uppercase letter and a lowercase letter"
-      );
+    toast.success("login successfully");
   };
   return (
     <div className="w-11/12 max-w-lg mx-auto py-12">
@@ -33,7 +39,7 @@ const Register = () => {
       <h2 className="text-xl font-bold text-center mb-4">
         Create a New Account
       </h2>
-      <form onSubmit={handleRegister} className="space-y-4">
+      <form onSubmit={handleSubmit(handleRegister)} className="space-y-3">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Name</span>
@@ -44,9 +50,13 @@ const Register = () => {
               placeholder="Name"
               className="input input-bordered w-full pl-10"
               name="name"
+              {...register("name", { required: "Name is required" })}
             />
             <FaUser className="absolute top-3 left-3 text-gray-400 z-10" />
           </div>
+          {errors.name && (
+            <p className="text-red-500 text-sm mt-1">{errors.name.message}</p>
+          )}
         </div>
         <div className="form-control">
           <label className="label">
@@ -58,9 +68,13 @@ const Register = () => {
               placeholder="Email"
               className="input input-bordered w-full pl-10"
               name="email"
+              {...register("mail", { required: "Email Address is required" })}
             />
             <FaEnvelope className="absolute top-3 left-3 text-gray-400 z-10" />
           </div>
+          {errors.mail && (
+            <p className="text-red-500 text-sm mt-1">{errors.mail.message}</p>
+          )}
         </div>
         <div className="form-control">
           <label className="label">
@@ -86,6 +100,14 @@ const Register = () => {
               placeholder="Password"
               className="input input-bordered w-full pl-10"
               name="password"
+              {...register("password", {
+                required: "Password is required",
+                pattern: {
+                  value: /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/,
+                  message:
+                    "Password must be at least 6 characters with uppercase and lowercase letters",
+                },
+              })}
             />
             <FaLock className="absolute top-3 left-3 text-gray-400 z-10" />
             {showPassword ? (
@@ -100,6 +122,11 @@ const Register = () => {
               />
             )}
           </div>
+          {errors.password && (
+            <p className="text-red-500 text-sm mt-1">
+              {errors.password.message}
+            </p>
+          )}
         </div>
         <div className="flex justify-between items-center">
           <label className="label cursor-pointer">
