4da321c7ad3bbd695eea6994f0c594140a8f0dde
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-06-08 16:40:21 +0600

    show loading spinner when route changes

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import Header from "../components/Header/Header";
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
 import Footer from "../components/Footer/Footer";
+import Spinner from "../components/ui/Spinner";
 
 const MainLayout = () => {
+  const navigation = useNavigation();
+  const isNavigating = Boolean(navigation.location);
   return (
     <div className="font-barlow">
       <Header />
-      <div className="mt-[64px] min-h-[calc(100vh-433px)]">
-        <Outlet />
-      </div>
+      {isNavigating ? (
+        <Spinner />
+      ) : (
+        <div className="mt-[64px] min-h-[calc(100vh-433px)]">
+          <Outlet />
+        </div>
+      )}
       <Footer />
     </div>
   );
