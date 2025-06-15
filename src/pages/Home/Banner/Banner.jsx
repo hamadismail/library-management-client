c065f24744ac2a3b0567e4cc6dd3f00b5bc75a76
Hamad Ismail <hamadismail@Hamads-iMac-Pro.local>
2025-06-15 10:29:43 +0600

    reduce z index of banner navigation button

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -81,7 +81,7 @@ const Banner = () => {
       </Swiper>
 
       {/* Custom Navigation */}
-      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 z-10 flex items-center gap-6">
+      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 z-9 flex items-center gap-6">
         <button
           ref={prevRef}
           className="cursor-pointer w-12 h-12 rounded-full bg-white/90 shadow-md flex items-center justify-center hover:bg-white transition-all duration-300"
